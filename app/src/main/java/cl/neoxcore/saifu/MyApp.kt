ea23def11bc91cb3d15b2a9d7bed1bf113d0a95a
Fabian Baez <neoxleonhart@gmail.com>
2021-07-22 18:49:17 -0400

    App, MainActivity and some libraries added

---
+++ app/src/main/java/cl/neoxcore/saifu/MyApp.kt
@@ -0,0 +1,6 @@
+package cl.neoxcore.saifu
+
+import android.app.Application
+
+class MyApp : Application() {
+}
---
+++ app/src/main/java/cl/neoxcore/saifu/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package cl.neoxcore.saifu.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cl.neoxcore.saifu.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
